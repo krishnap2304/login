1b15e5b643e44f241b9ae634dcbca8eeb40bfb74
Pilllem Krishna Prasad <pilprasa@Pilllems-MacBook-Pro.local>
2021-06-18 14:15:03 +0530

    modified code

--- src/main/java/com/bootcamp/login/security/Test.java
+++ src/main/java/com/bootcamp/login/security/Test.java
@@ -1,14 +1,10 @@
 package com.bootcamp.login.security;
-import com.bootcamp.login.repository.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Test {
-    @Autowired
-    RoleRepository roleRepository;
 
     public static void main(String[] args) {
-        String password = "123456";
+        String password = "Kpjenkins4321";
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
         System.out.println(hashedPassword);
